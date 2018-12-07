93cfc562d820aa5d72eeda22e533f3afa5e581b2
DragonGeek27 <dragongeek27@gmail.com>
2018-12-07 13:05:26 -0700

    1.0
    1.0

---
+++ VR Beer Pong Ver 1.0/VR Beer Pong Ver 1.0/Library/Collab/Download/Assets/script/DestroyBall2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBall2 : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+
+    void OnTriggerEnter(Collider other)
+    {
+		
+		GameObject.Find ("ballSpawner1").GetComponent<SpawnBall> ().spawnMe = true;
+		Destroy(gameObject);
+
+    }
+
+
+}
